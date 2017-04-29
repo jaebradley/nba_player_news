6f83aab7a32f197bb527f5fc388934ae1ccc3489
jaebradley <jae.b.bradley@gmail.com>
2017-04-29 19:46:55 -0400

    inserter

--- nba_player_news/inserters.py
+++ nba_player_news/inserters.py
@@ -1,14 +1,17 @@
+import os
 from nba_data import Client
 import redis
+
 
 class RotoWireInserter:
 
     def __init__(self):
-        redis_client = redis.StrictRedis()
+        self.redis_client = redis.StrictRedis(host=os.environ['REDIS_HOST'], port=os.environ['REDIS_PORT'], db=0)
 
     def insert(self):
         for player_news_item in Client.get_player_news():
-
+            self.redis_client.set(name=RotoWireInserter.calculate_key_id(player_news_item=player_news_item),
+                                  value=player_news_item)
 
     @staticmethod
     def calculate_key_id(player_news_item):
---
+++ settings.py
@@ -0,0 +1,4 @@
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+load_dotenv(join(dirname(__file__), '.env'))
