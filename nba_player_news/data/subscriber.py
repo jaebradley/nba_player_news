1898d64110c4c0f25519ddc8345f32b122bf2952
jaebradley <jae.b.bradley@gmail.com>
2017-04-30 08:51:51 -0400

    add redis subscriber

---
+++ nba_player_news/data/subscriber.py
@@ -0,0 +1,27 @@
+import logging
+import logging.config
+import os
+
+import redis
+
+from environment import REDIS_HOST, REDIS_PORT, REDIS_CHANNEL_NAME
+
+
+class Subscriber:
+    logging.config.fileConfig(os.path.join(os.path.dirname(__file__), "../../logger.conf"))
+    logger = logging.getLogger("subscriber")
+
+    def __init__(self):
+        self.redis_client = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=0)
+        self.publisher_subscriber = self.redis_client.pubsub()
+        self.publisher_subscriber.subscribe(REDIS_CHANNEL_NAME)
+
+    def process_messages(self):
+        while True:
+            message = self.publisher_subscriber.get_message()
+            if message:
+                self.process_message(message=message)
+
+    def process_message(self, message):
+        Subscriber.logger.info("Processing message with pattern: {pattern} | type: {type} | channel: {channel} | data: {data}".format(**message))
+
