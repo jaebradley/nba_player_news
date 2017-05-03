44926b6f901b52bb84fb9f028c2127ba37fb3a2e
jaebradley <jae.b.bradley@gmail.com>
2017-05-02 23:54:48 -0400

    add logging

--- nba_player_news/views.py
+++ nba_player_news/views.py
@@ -1,11 +1,18 @@
 from django.http import HttpResponse, HttpResponseForbidden
+import logging
+import logging.config
+import os
 
 from environment import FACEBOOK_VERIFY_TOKEN
 from models import Subscription
 
+logging.config.fileConfig(os.path.join(os.path.dirname(__file__), "../logger.conf"))
+logger = logging.getLogger("subscriber")
+
 
 def facebook(request):
     if request.method == "GET":
+        logger.info("Arguments: {}".format(request.GET))
         if request.GET.get("hub.mode") == "subscribe" and "hub.challenge" in request.GET:
             if not request.GET.get("hub.verify_token") == FACEBOOK_VERIFY_TOKEN:
                 return HttpResponseForbidden("Verification token mismatch")
@@ -13,12 +20,15 @@ def facebook(request):
 
     if request.method == "POST":
         if request.POST.get("object") == "page":
+            logger.info("Arguments: {}".format(request.POST))
             for entry in request.POST.get("entry"):
                 for message_event in entry["messaging"]:
                     if "message" in message_event:
                         sender_id = message_event["sender"]["id"]
                         recipient_id = message_event["recipient"]["id"]
                         message_text = message_event["message"]["text"]
-                        Subscription.objects.create(platform="facebook", platform_identifier=sender_id)
+                        logger.info("Sender: {} | Recipient: {} | Message: {}".format(sender_id, recipient_id, message_text))
+                        if message_text.lower == "subscribe":
+                            Subscription.objects.create(platform="facebook", platform_identifier=sender_id)
 
     return HttpResponse("ok")
