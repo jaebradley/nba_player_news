772976cf465624cdf1209dbc3af5610a6e27895a
jaebradley <jae.b.bradley@gmail.com>
2017-05-03 01:37:11 -0400

    update

--- nba_player_news/settings.py
+++ nba_player_news/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'nba_player_news',
     'graphene_django',
     'django_filters',
+    "sslserver",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- nba_player_news/views.py
+++ nba_player_news/views.py
@@ -1,6 +1,7 @@
 import logging
 import logging.config
 import os
+import json
 
 from django.http import HttpResponse, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
@@ -10,7 +11,7 @@ from models import Subscription
 from nba_player_news.data.senders import FacebookMessager
 
 logging.config.fileConfig(os.path.join(os.path.dirname(__file__), "../logger.conf"))
-logger = logging.getLogger("subscriber")
+logger = logging.getLogger("views")
 
 
 @csrf_exempt
@@ -23,9 +24,10 @@ def facebook(request):
         return HttpResponse(request.GET.get("hub.challenge"))
 
     if request.method == "POST":
-        if request.POST.get("object") == "page":
-            logger.info("Arguments: {}".format(request.POST))
-            for entry in request.POST.get("entry"):
+        body = json.loads(request.body)
+        logger.info("Arguments: {}".format(body))
+        if body.get("object") == "page":
+            for entry in body.get("entry"):
                 for message_event in entry["messaging"]:
                     if "message" in message_event:
                         sender_id = message_event["sender"]["id"]
@@ -33,7 +35,7 @@ def facebook(request):
                         message_text = message_event["message"]["text"]
                         logger.info("Sender: {} | Recipient: {} | Message: {}".format(sender_id, recipient_id, message_text))
                         if message_text.lower() == "subscribe":
-                            Subscription.objects.create(platform="facebook", platform_identifier=sender_id)
+                            Subscription.objects.get_or_create(platform="facebook", platform_identifier=sender_id)
                             FacebookMessager().send(recipient_id=recipient_id, message="You are now subscribed")
 
     return HttpResponse("ok")
