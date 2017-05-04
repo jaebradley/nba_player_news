3b9d03877e7b872c68d1a7e36df8ea67d59abd67
jaebradley <jae.b.bradley@gmail.com>
2017-05-03 22:52:31 -0400

    initial subscriber implementation

--- nba_player_news/data/subscribers.py
+++ nba_player_news/data/subscribers.py
@@ -6,7 +6,7 @@ import os
 
 import redis
 
-from environment import REDIS_URL, REDIS_CHANNEL_NAME
+from environment import REDIS_URL, REDIS_CHANNEL_NAME, REDIS_SUBSCRIBER_EVENTS_CHANNEL_NAME
 from nba_player_news.data.platform_subscriptions_publishers import EmailSubscriptionsPublisher, TwitterSubscriptionsPublisher, FacebookMessengerSubscriptionsPublisher
 
 
@@ -37,3 +37,23 @@ class PlayerNewsSubscriber:
         # Ugh spam
         # self.email_subscriptions_publisher.publish(message=json.loads(message["data"]))
         self.facebook_messenger_subscriptions_publisher.publish(message=json.loads(message["data"]))
+
+
+class SubscriptionEventsSubscriber:
+    logging.config.fileConfig(os.path.join(os.path.dirname(__file__), "../../logger.conf"))
+    logger = logging.getLogger("subscriber")
+
+    def __init__(self):
+        self.redis_client = redis.StrictRedis().from_url(url=REDIS_URL)
+        self.publisher_subscriber = self.redis_client.pubsub()
+        self.publisher_subscriber.subscribe(REDIS_SUBSCRIBER_EVENTS_CHANNEL_NAME)
+
+    def process_messages(self):
+        while True:
+            message = self.publisher_subscriber.get_message()
+            if message and message["type"] == "message":
+                self.process_message(message=message)
+
+    def process_message(self, message):
+        if message.platform == "facebook":
+            return
