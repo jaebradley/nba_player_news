2e4c3d633f0167ab3f38c7575fbb7025083520ae
jaebradley <jae.b.bradley@gmail.com>
2017-05-04 23:54:07 -0400

    update view

--- nba_player_news/views.py
+++ nba_player_news/views.py
@@ -1,17 +1,17 @@
 import logging
 import logging.config
 import os
-import json
 
+import redis
 from django.http import HttpResponse, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
 
-from environment import FACEBOOK_VERIFY_TOKEN
-from models import Subscription
-from nba_player_news.data.senders import FacebookMessager
+from environment import FACEBOOK_VERIFY_TOKEN, REDIS_SUBSCRIBER_EVENTS_CHANNEL_NAME, REDIS_URL
 
 logging.config.fileConfig(os.path.join(os.path.dirname(__file__), "../logger.conf"))
 logger = logging.getLogger("views")
+
+client = redis.StrictRedis().from_url(url=REDIS_URL)
 
 
 @csrf_exempt
@@ -24,18 +24,6 @@ def facebook(request):
         return HttpResponse(request.GET.get("hub.challenge"))
 
     if request.method == "POST":
-        body = json.loads(request.body)
-        logger.info("Arguments: {}".format(body))
-        if body.get("object") == "page":
-            for entry in body.get("entry"):
-                for message_event in entry["messaging"]:
-                    if "message" in message_event:
-                        sender_id = message_event["sender"]["id"]
-                        recipient_id = message_event["recipient"]["id"]
-                        message_text = message_event["message"]["text"]
-                        logger.info("Sender: {} | Recipient: {} | Message: {}".format(sender_id, recipient_id, message_text))
-                        if message_text.lower() == "subscribe":
-                            Subscription.objects.get_or_create(platform="facebook", platform_identifier=sender_id)
-                            FacebookMessager().send(recipient_id=sender_id, message="You are now subscribed")
+        client.publish(channel=REDIS_SUBSCRIBER_EVENTS_CHANNEL_NAME, message=request.body)
 
     return HttpResponse("ok")
