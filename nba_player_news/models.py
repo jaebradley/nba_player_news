0d52eed7fb20b412b710eb869f930e306e55e034
jaebradley <jae.b.bradley@gmail.com>
2017-05-07 10:03:57 -0400

    add subscription attempts models

--- nba_player_news/models.py
+++ nba_player_news/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 # Create your models here.
 
 
-from django.db.models import Model, CharField, DateTimeField
+from django.db.models import Model, CharField, DateTimeField, ForeignKey, CASCADE, TextField, BooleanField
 
 
 class Subscription(Model):
@@ -22,3 +22,25 @@ class Subscription(Model):
     def __unicode__(self):
         return "platform: {} | platform_identifier: {} | created_at: {} | updated_at: {} | unsubscribed_at: {}"\
             .format(self.platform, self.platform_identifier, self.created_at, self.updated_at, self.unsubscribed_at)
+
+
+class SubscriptionAttempt(Model):
+    subscription = ForeignKey(to=Subscription, on_delete=CASCADE)
+    attempted_at = DateTimeField(auto_now_add=True)
+    message = TextField(max_length=2048)
+
+    def __unicode__(self):
+        return "Subscription: {subscription} | Attempted at: {attempted_at} | Message: {message}"\
+            .format(subscription=self.subscription, attempted_at=self.attempted_at, message=self.message)
+
+
+class SubscriptionAttemptResult(Model):
+    subscription_attempt = ForeignKey(to=SubscriptionAttempt, on_delete=CASCADE)
+    resolved_at = DateTimeField(auto_now_add=True)
+    successful = BooleanField
+    response = TextField(max_length=2048)
+
+    def __unicode__(self):
+        return "Subscription Attempt: {subscription_attempt} | Resolved At: {resolved_at} | Successful: {successful} | Response: {response}"\
+            .format(subscription_attempt=self.subscription_attempt, resolved_at=self.resolved_at,
+                    successful=self.successful, response=self.response)
