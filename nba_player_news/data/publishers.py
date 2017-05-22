33a37041f29c4778057969b7fd10965da821e5df
jaebradley <jae.b.bradley@gmail.com>
2017-05-21 22:08:02 -0400

    remove email and twitter publishers

--- nba_player_news/data/publishers.py
+++ nba_player_news/data/publishers.py
@@ -113,33 +113,3 @@ class PlayerNewsSubscriptionMessagePublisher:
         PlayerNewsSubscriptionMessagePublisher.logger.info("Published to {subscriber_count} subscribers: {message}"
                                                            .format(subscriber_count=subscriber_count,
                                                                    message=subscription_message.to_json()))
-
-
-class EmailSubscriptionsPublisher:
-    logger = logging.getLogger("subscriptionMessagePublisher")
-
-    def __init__(self):
-        self.emailer = Emailer()
-
-    def publish(self, message):
-        for subscription in Subscription.objects.filter(platform="email", unsubscribed_at=None):
-            EmailSubscriptionsPublisher.logger.info("Publishing message: {} to subscription: {}"
-                                                    .format(message, subscription))
-            self.emailer.send(destination=subscription.platform_identifier, message=message)
-
-
-class TwitterSubscriptionsPublisher:
-    logger = logging.getLogger("subscriptionMessagePublisher")
-
-    def __init__(self):
-        auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
-        auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
-        self.client = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
-        self.tweeter = Tweeter()
-
-    def publish(self, message):
-        for page in tweepy.Cursor(self.client.followers_ids, id="nba_player_news", count=200).pages():
-            for follower_id in page:
-                TwitterSubscriptionsPublisher.logger.info("Publishing message: {} to follower: {}"
-                                                          .format(message, follower_id))
-                self.tweeter.send(user_id=follower_id, message=message)
